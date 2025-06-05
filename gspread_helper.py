f913a9ec39496a6f65d880c1f1b82d507e2e70ff
Ricky Damazio <119544239+RickyT93@users.noreply.github.com>
2025-06-05 01:30:34 -0500

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -1,8 +1,8 @@
 import gspread
 from datetime import datetime
 
-def get_gsheet_connection(creds_path):
-    return gspread.service_account(filename=creds_path)
+def get_gsheet_connection(creds_dict):
+    return gspread.service_account_from_dict(creds_dict)
 
 def log_workout(gc, sheet_url, day_type, exercises, logs):
     sheet = gc.open_by_url(sheet_url).sheet1
