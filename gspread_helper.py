2cf21ba7d04d8e2733e25e1de289310e2835c300
Ricky Damazio <119544239+RickyT93@users.noreply.github.com>
2025-06-05 19:51:06 -0500

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -1,11 +1,17 @@
 import gspread
-from datetime import datetime
+import streamlit as st
 
-def get_gsheet_connection(creds_dict):
+def get_gsheet_connection():
+    creds_dict = {
+        "type": st.secrets["gspread"]["type"],
+        "project_id": st.secrets["gspread"]["project_id"],
+        "private_key_id": st.secrets["gspread"]["private_key_id"],
+        "private_key": st.secrets["gspread"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["gspread"]["client_email"],
+        "client_id": st.secrets["gspread"]["client_id"],
+        "auth_uri": st.secrets["gspread"]["auth_uri"],
+        "token_uri": st.secrets["gspread"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["gspread"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["gspread"]["client_x509_cert_url"]
+    }
     return gspread.service_account_from_dict(creds_dict)
-
-def log_workout(gc, sheet_url, day_type, exercises, logs):
-    sheet = gc.open_by_url(sheet_url).sheet1
-    date = datetime.today().strftime("%Y-%m-%d")
-    for ex, log in zip(exercises, logs):
-        sheet.append_row([date, day_type, ex["name"], ex["muscle"], ex["equipment"], log])
