1957ba5a968269d8dc183af19d40c1ac72c86b3a
Ricky Damazio <119544239+RickyT93@users.noreply.github.com>
2025-07-04 16:54:26 -0500

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -3,15 +3,15 @@ import streamlit as st
 
 def get_gsheet_connection():
     creds_dict = {
-        "type": st.secrets["gspread"]["type"],
-        "project_id": st.secrets["gspread"]["project_id"],
-        "private_key_id": st.secrets["gspread"]["private_key_id"],
-        "private_key": st.secrets["gspread"]["private_key"].replace("\\n", "\n"),
-        "client_email": st.secrets["gspread"]["client_email"],
-        "client_id": st.secrets["gspread"]["client_id"],
-        "auth_uri": st.secrets["gspread"]["auth_uri"],
-        "token_uri": st.secrets["gspread"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gspread"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["gspread"]["client_x509_cert_url"]
+        "type": st.secrets["gspread_creds"]["type"],
+        "project_id": st.secrets["gspread_creds"]["project_id"],
+        "private_key_id": st.secrets["gspread_creds"]["private_key_id"],
+        "private_key": st.secrets["gspread_creds"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["gspread_creds"]["client_email"],
+        "client_id": st.secrets["gspread_creds"]["client_id"],
+        "auth_uri": st.secrets["gspread_creds"]["auth_uri"],
+        "token_uri": st.secrets["gspread_creds"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["gspread_creds"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["gspread_creds"]["client_x509_cert_url"]
     }
     return gspread.service_account_from_dict(creds_dict)
